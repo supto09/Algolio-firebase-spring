a454d2ac73b15b3af3b0c12dc2a214bae9ea4364
Ashik Saeed <supto09apee@gmail.com>
2022-04-07 15:47:45 +0600

    firebase connection verified

--- backend/src/main/kotlin/example/firebase/algolio/TestController.kt
+++ backend/src/main/kotlin/example/firebase/algolio/TestController.kt
@@ -1,12 +1,23 @@
 package example.firebase.algolio
 
+import com.google.firebase.auth.FirebaseAuth
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+
 
 @RestController
 class TestController {
     @GetMapping("/")
-    fun hello():String{
+    fun hello(): String {
+
+        var page = FirebaseAuth.getInstance().listUsers(null)
+        while (page != null) {
+            for (user in page.values) {
+                println("User: " + user.uid)
+            }
+            page = page.nextPage
+        }
+
         return "Hello mars 7";
     }
 }
